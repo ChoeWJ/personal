e92774f85ca6e6780e238a5e5824c78c6cc3756e
choewj <choewj117@gmail.com>
2025-03-06 17:33:28 +0900

    convert between celsius and fahrenheit with rust

--- folium/01_basic_map.py
+++
@@ -1,5 +0,0 @@
-import folium
-
-m = folium.Map(location=[37.5665, 126.9780], zoom_start=12)
-
-m.save('folium/01_basic_map.html')
--- folium/03_add_marker.py
+++
@@ -1,7 +0,0 @@
-import folium
-
-m = folium.Map(location=[37.5665, 126.9780], zoom_start=12)
-
-folium.Marker([37.5665, 126.9780], popup="서울", tooltip="클릭하세요!").add_to(m)
-
-m.save("folium/03_marker.html")
